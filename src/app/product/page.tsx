a8d5e63756d945e2fa14893b6869499be2603824
Joao <junioralmeida_15@hotmail.com>
2025-08-04 21:51:45 -0300

    initial setup: eslint, prettier and shadcn components

---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Product = () => {
+    return (
+        <div>
+            <h1>Product</h1>
+            <Button>Teste</Button>
+        </div>
+    );
+};
+
+export default Product;
