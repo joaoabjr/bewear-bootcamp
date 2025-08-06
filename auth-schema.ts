7fad0931485f9175e8a510a4e154b6271ee3e55e
Joao <junioralmeida_15@hotmail.com>
2025-08-06 12:51:52 -0300

    feat: google sigin

--- auth-schema.ts
+++ auth-schema.ts
@@ -53,15 +53,15 @@ export const accountTable = pgTable("account_table", {
   updatedAt: timestamp("updated_at").notNull(),
 });
 
-// export const verification = pgTable("verification", {
-//   id: text("id").primaryKey(),
-//   identifier: text("identifier").notNull(),
-//   value: text("value").notNull(),
-//   expiresAt: timestamp("expires_at").notNull(),
-//   createdAt: timestamp("created_at").$defaultFn(
-//     () => /* @__PURE__ */ new Date(),
-//   ),
-//   updatedAt: timestamp("updated_at").$defaultFn(
-//     () => /* @__PURE__ */ new Date(),
-//   ),
-// });
+export const verification = pgTable("verification", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+  updatedAt: timestamp("updated_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+});
